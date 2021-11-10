eb75e8e5287df80799bb3d89b94369c1715f08bb
Shivanshu <blah92163@gmail.com>
2021-11-10 19:33:37 +0530

    enabled CORS using npm's cors package

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import morgan from 'morgan';
 import expenseRouter from './routes/expenseRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -13,7 +14,7 @@ if(process.env.NODE_ENV === 'development')
 }   
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}`));
 
